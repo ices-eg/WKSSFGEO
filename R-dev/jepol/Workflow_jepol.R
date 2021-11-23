7da5921407bd40b92e971ad42b516f746a42f1fa
Jeppe Olsen <jepol@aqua.dtu.dk>
2021-11-23 14:55:20 +0100

    Add files via upload

--- R-dev/jepol/Workflow_jepol.R
+++ R-dev/jepol/Workflow_jepol.R
@@ -1,40 +1,55 @@
 rm(list=ls())
-#if (!require(pacman)){
-#  install.packages("pacman", repos="http://ftp.ussg.iu.edu/CRAN/")
-#  library(pacman)
-#}
-#pacman::p_load(data.table, sf, mapview, devtools, readr)
-library(magrittr)
+if (!require(pacman)){
+  install.packages("pacman", repos="http://ftp.ussg.iu.edu/CRAN/")
+  library(pacman)
+}
+p_load(data.table, sf, mapview, devtools, readr, dplyr)
+
 
 # Download example file
 
-githubURL1 <- "https://raw.githubusercontent.com/ices-eg/WKSSFGEO/main/example_data_AIS.csv"
-dat <- 
-  readr::read_csv(githubURL1) %>% 
-  dplyr::mutate(vid = stringr::str_sub(vessel_id, 4) %>% as.integer()) %>% 
-  dplyr::arrange(vessel_id, time_stamp) %>% 
-  dplyr::mutate(.rid = 1:dplyr::n())
-data.table::setDT(dat)
+githubURL1 <- "https://raw.githubusercontent.com/ices-eg/WKSSFGEO/main/data-examples/example_data_AIS.csv"
+dat <- readr::read_csv(githubURL1)
+setDT(dat)
 dat
 
 #Download the harbours file to you desk and load it to the r environment:
-#https://github.com/ices-eg/WKSSFGEO/blob/main/jepol/harbours.rds
-
-hbs <- readRDS("jepol/harbours.rds")
+#https://github.com/ices-eg/WKSSFGEO/blob/main/data/harbours.rds
+hbs <- readRDS("//aqua-cp-jepol18/C$/Users/jepol/Downloads/harbours.rds")
 
 #Load the function define_trips.R
-#devtools::source_url("https://raw.githubusercontent.com/ices-eg/WKSSFGEO/main/jepol/define_trips.R")
-source("jepol/define_trips.R")
-#Use function to extract the trips from the dataset
-out <- define_trips_pol(dat)
+devtools::source_url("https://raw.githubusercontent.com/ices-eg/WKSSFGEO/main/R-dev/jepol/define_trips_pol.R")
+devtools::source_url("https://raw.githubusercontent.com/ices-eg/WKSSFGEO/main/R-dev/jepol/add_harbours.R")
+devtools::source_url("https://raw.githubusercontent.com/ices-eg/WKSSFGEO/main/R-dev/jepol/interpolate_ais.R")
 
+# dat2 <- 
+# dat %>%
+#   dplyr::group_by(vessel_id) %>%
+#   dplyr::mutate(whacky = argosfilter::vmask(lat, lon, time_stamp, vmax = 12))
+
+
+
+out1 <- add_harbours(dat, hbs)
+out11 <- interpolate_ais(out1)
+table(out11$source)
+
+#Use function to extract the trips from the dataset
+out2 <- define_trips_pol(out11, min_dur = 0.8, max_dur = 48, 
+                        split_trips = T, preserve_all = F)
+
+schedule <- out2[,.(depart = min(time_stamp), return = max(time_stamp))
+                , by = .(vessel_id, trip_id)]
+schedule <- schedule[!is.na(trip_id)]
+schedule[, duration_hrs := as.numeric(difftime(return, depart, units = "hours"))]
+schedule
 
 #Look at a single trip
-tst <- out[trip_id == unique(out$trip_id)[20]]
+tst <- out2[trip_id == unique(out2$trip_id)[30]]
+tst <- out2[trip_id == "EX_10_1"]
 
 pts <- tst %>%
-  sf::st_as_sf(coords = c("lon","lat"),
-               crs = 4326)
-mapview::mapview(pts)
+  sf::st_as_sf(coords = c("lon","lat")) %>%
+  sf::st_set_crs(4326)
 
+mapview(pts, zcol = "source")
 
